08ae2639b525f133d5bf8e9b24a560366cb48635
Max Edell <maxed@adobe.com>
2023-08-16 14:23:21 -0700

    columns block

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,18 +1,14 @@
-export default function decorate(block) {
-  const cols = [...block.firstElementChild.children];
-  block.classList.add(`columns-${cols.length}-cols`);
-
-  // setup image columns
-  [...block.children].forEach((row) => {
-    [...row.children].forEach((col) => {
-      const pic = col.querySelector('picture');
-      if (pic) {
-        const picWrapper = pic.closest('div');
-        if (picWrapper && picWrapper.children.length === 1) {
-          // picture is only content in column
-          picWrapper.classList.add('columns-img-col');
-        }
-      }
+export default function init(el) {
+  const rows = el.querySelectorAll(':scope > div');
+  rows.forEach((row, rdx) => {
+    row.className = `row row-${rdx + 1}`;
+    const cols = row.querySelectorAll(':scope > div');
+    cols.forEach((col, cdx) => {
+      col.className = `col col-${cdx + 1}`;
     });
   });
+  if (el.classList.contains('table')) {
+    el.classList.add('columns-table');
+    el.classList.remove('table');
+  }
 }
